fb8d5635e9d57f0b3c8cb8144cdcb84ba10fa2fc
ATAGWE ROGER <atagweroger2@gmail.com>
2025-08-29 12:27:29 +0100

    Added a header component to the Dashboard component as a child this holds the website title and welcome text

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <>
+      <header class="chronicles-header">
+      <h1 class="chronicles-title">Chronicles</h1>
+      <p class="chronicles-subtitle">
+        Dive into stories, thoughts, and ideas from around the world
+      </p>
+    </header>
+        </>
+    )
+}
+
+export default Header;
